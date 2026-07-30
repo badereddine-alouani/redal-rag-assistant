5cafbf7e42e59814cbd419b9c51c3dd52f0a2f93
badereddine-alouani <badereddine.elalouani@gmail.com>
2026-07-30 19:37:42 +0100

    feat(widget): release embeddable widget to production

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -129,7 +129,7 @@ function App() {
       if (!isFallback) {
         setTimeout(() => {
           addBotMessage("Avez-vous une autre question ?");
-        }, 1001);
+        }, 1000);
       }
 
     } catch (error) {
